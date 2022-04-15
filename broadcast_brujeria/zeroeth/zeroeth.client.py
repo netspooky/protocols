e62e9a097da8a4accf3ff88fa7027d727d787943
【☆ ゆう ☆ 】 <26436276+netspooky@users.noreply.github.com>
2022-04-15 19:59:24 -0400

    Create zeroeth.client.py

---
+++ broadcast_brujeria/zeroeth/zeroeth.client.py
@@ -0,0 +1,24 @@
+import socket
+import time
+
+# LISTEN.PY - Simple null frame transport over ethernet listener for ZEROETH
+# Run:
+# $ sudo python3 zeroeth.client.py
+# On the remote computer:
+# $ cat file | sudo python3 zeroeth.py
+
+s = socket.socket( socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(0x0003))
+timeout = time.time() + 10 # Time limit for listener
+
+while time.time() < timeout:
+    try:
+        packet  = s.recvfrom(65565) # The buffer
+        packet  = packet[0]     # Our packet
+        ethtype = packet[12:14] # The ethernet type
+
+        #if ethtype == b"\x00\x00": # Match the ethernet type
+        if ethtype == b"\x5a\x45": # Match the ethernet type
+            decodedpkt = len(packet) - 0x40
+            print("{}".format(chr(decodedpkt)), end="")
+    except Exception as e:
+        print(e)
